caa2e32a9e65b00d1166b9c39041decef0730199
= <=>
2023-02-14 11:23:03 -0300

    login btn working

--- frontend-ap-pf/src/app/components/sections/nav-bar/nav-bar.component.ts
+++ frontend-ap-pf/src/app/components/sections/nav-bar/nav-bar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
+  constructor (private router:Router) {}
 
+  ngOnInit():void {}
+
+  login(){
+    this.router.navigate(['/login']);
+  }
 }
