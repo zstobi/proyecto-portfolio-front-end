200217a5b33fbf323eb5dfa4796dc062d13a3301
= <=>
2023-02-16 23:35:16 -0300

    few changes

--- frontend-ap-pf/src/app/components/sections/prjs/prjs.component.ts
+++ frontend-ap-pf/src/app/components/sections/prjs/prjs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-prjs',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./prjs.component.css']
 })
 export class PrjsComponent {
+  constructor(
+    private router: Router
+  ) {}
 
+  ngOnInit(){
+    this.editMode();
+  }
+
+  editMode(){
+    if (localStorage.getItem('token') == null){
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
